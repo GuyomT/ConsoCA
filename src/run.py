433d2fb5a84acafae32bfc357b5211d9fa5dd394
Guillaume <guillaume.terriere@epitech.eu>
2023-12-10 18:09:28 -0500

    Add real-time data, database setup, and data visualization

--- src/run.py
+++ src/run.py
@@ -22,8 +22,8 @@ def getLastRecord():
 if __name__ == '__main__':
     energy_data = EnergyDataHarvest()
     energy_data.getArchivedData()
-    energy_data.getRealTimeData()
-    energy_data.plotDataFromPeriod('2021-01-01', '2021-01-02', 'quebec')
-    energy_data.plotBarChartFromPeriod('2021-01-01', '2021-01-02', 'quebec')
-    energy_data.plotComparisonBetweenYears('2020', '2021', 'quebec')
-    getLastRecord()
+    # energy_data.getRealTimeData()
+    # energy_data.plotDataFromPeriod('2021-01-01', '2021-01-02', 'quebec')
+    # energy_data.plotBarChartFromPeriod('2021-01-01', '2021-01-02', 'quebec')
+    # energy_data.plotComparisonBetweenYears('2020', '2021', 'quebec')
+    # getLastRecord()
